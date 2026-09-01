7d4138762651e03d507fc923e993a6f3fe5e8dee
Blazeking300 <95568843+Blazeking300@users.noreply.github.com>
2026-09-01 11:17:01 +0100

    Delete Projectile motion simulator directory

--- Projectile motion simulator/Projectile-Motion-Code-Dev/classes/__init__.py
+++
@@ -1,6 +0,0 @@
-# Classes package initialization
-from classes.projectile import Projectile
-from classes.target import Target
-from classes.barrier import Barrier
-from classes.simulation import Simulation
-from classes.ui import UI
--- Projectile motion simulator/Projectile-Motion-Code-Dev/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-# Utils package initialization
-from utils.constants import *
-from utils.physics import PhysicsEngine
